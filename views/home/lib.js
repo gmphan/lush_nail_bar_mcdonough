b469815adfb9017c09f26636ecb0eccfc9e9b227
Giang Phan <giangphan@GP-Mac13.local>
2019-08-28 22:48:40 -0400

    added nodemailer

--- views/home/lib.js
+++ views/home/lib.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     alert('testing')
-    // $('#home-form-1').submit(function(e){
-    //     e.preventDefault();
-    //     console.log('submit function hit');
-    // });
+    $('#home-form-1').submit(function(e){
+        e.preventDefault();
+        console.log('submit function hit');
+    });
 });
