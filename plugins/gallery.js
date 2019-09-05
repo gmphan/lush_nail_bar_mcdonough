acefdc32fe3952ec15615f51f081aa6ad52d34f4
Giang Phan <giangphan@GP-Mac13.local>
2019-09-04 21:19:30 -0400

    getting photo name for display

--- plugins/gallery.js
+++ plugins/gallery.js
@@ -1,8 +1,20 @@
 'use strict'
-
+const path = require('path')
+const fs    = require('fs')
 const log = require('../lib/logger')
 
 async function handleHomePage(req, reply){
+
+    const galleryPath = path.join(__dirname, '../public/assets/img', 'gallery')
+    const gallery = fs.readdirSync(galleryPath)  
+    // console.log(photos)
+    for(let photo of gallery){
+        const photoPath = path.join(__dirname, '../public/assets/img', 'gallery', photo)  
+        const photoName = path.parse(photoPath).name
+        console.log(photoName)
+    }
+
+
     reply.view('/views/gallery/index', {text:'text'})
     // reply.send('home page')
 }
