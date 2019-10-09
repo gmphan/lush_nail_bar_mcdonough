30e1196a7b142a5da7ecc1a33232be466d1791c7
Giang Phan <giangphan@clayton.edu>
2019-10-09 13:47:11 -0400

    price-list

--- views/price-list/lib.js
+++ views/price-list/lib.js
@@ -1,11 +1,15 @@
-$('.text_container').addClass("hidden");
-$('.text_container').click(function() {
-    var $this = $(this);
+// $('.text_container').addClass("hidden");
+// $('.text_container').click(function() {
+//     var $this = $(this);
 
-    if ($this.hasClass("hidden")) {
-        $(this).removeClass("hidden").addClass("visible");
+//     if ($this.hasClass("hidden")) {
+//         $(this).removeClass("hidden").addClass("visible");
 
-    } else {
-        $(this).removeClass("visible").addClass("hidden");
-    }
-});
+//     } else {
+//         $(this).removeClass("visible").addClass("hidden");
+//     }
+// });
+$('.price-link').on("click", function(){
+    //get current active class
+    //get value which related to id content
+})
