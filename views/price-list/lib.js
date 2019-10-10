bb7cc09280e6e918b2de8a5679ced7bb25b19490
Giang Phan <giangphan@clayton.edu>
2019-10-10 09:48:36 -0400

    working on pricelist's lib

--- views/price-list/lib.js
+++ views/price-list/lib.js
@@ -1,3 +1,4 @@
+
 // $('.text_container').addClass("hidden");
 // $('.text_container').click(function() {
 //     var $this = $(this);
@@ -9,7 +10,26 @@
 //         $(this).removeClass("visible").addClass("hidden");
 //     }
 // });
+
+var crntActiveId = getCurrtPriceList()
 $('.price-link').on("click", function(){
+    
     //get current active class
     //get value which related to id content
 })
+
+
+
+function getCurrtPriceList(){
+    var pricelistElement = document.getElementById('price-list')
+    var pricelistList = pricelistElement.childNodes[1].getElementsByClassName('components')
+    var pricelistActive = pricelistList[0].getElementsByClassName('active')
+    var currentActHref = pricelistActive[0].childNodes[0].href
+    var numSymbolPos = currentActHref.search('#')
+    var crntActId = currentActHref.substring(numSymbolPos+1, currentActHref.length)
+    return crntActId
+    // console.log(crntActId)
+}
+function getDisplayClass(id){
+    
+}
