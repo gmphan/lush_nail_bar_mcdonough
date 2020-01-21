b12d86636a9c4efc0c6343b91f03412c99fd2c56
Giang Phan <giangphan@clayton.edu>
2020-01-21 00:53:28 -0500

    done with pricelist logic

--- plugins/home.js
+++ plugins/home.js
@@ -24,6 +24,85 @@ async function handleHomePage(req, reply){
             'SNS Full Set'                  : '$35+',
             'SNS Ombre'                     : '$45+',
             'SNS Full Set Ombre'            : '$50+'
+        },
+        gel_polish:{
+            'Gel Polish w/Manicure (Without Take Off)'  : '$25+',
+            'Gel Polish w/Maniicure (With Take Off)'    : '$30+',
+            'Gel Polish w/Pedicure'                     : '$38',
+            'Gel Polish French w/Manicure'              : '$30',
+            'Gel Polish French w/Pedicure'              : '$42',
+            'Gel Polish Change on Hands'                : '$20',
+            'Gel Polish Change on Feet'                 : '$25',
+            'Gel Polish Only'                           : '$20'
+        },
+        mani_pedi:{
+            'Pedicure'                      : '$23',
+            'Manicure'                      : '$15',
+            'Pedicure & Manicure'           : '$35'
+        },
+        additional:{
+            'Polish Change Hand Color'                  : '$8',
+            'Polish Change Hand French of American'     : '$12',
+            'Polish Change Toes Color'                  : '$10',
+            'Polish Change Toes French American'        : '$15',
+            'Nail Repair'                               : '$5+',
+            'Nails Cut Down Acrylic'                    : '$3+',
+            'Nails Soak Off Acrylic'                    : '$10',
+            'Nail Art Design'                           : '$5+',
+            'Paraffin Wax Hand'                         : '$8',
+            'Paraffin Wax Leg'                          : '$10',
+            'Take Off Gel Polish or SNS'                : '$10',
+            '2 Acrylic on Toes'                         : '$12',
+            'Chrome Additional'                         : '$25'
+        },
+        spa_pedi:{
+            '1. Regular Pedicure: Pepper mint soak in, nail shapping, cuticle care + callus removal, finish with soothing and massage noisturizing lotion, and polish.' : '$23',
+
+            'Combo Classic' : '$36',
+            '2. Classic Deluxe Pedicure: Pepper mint soak in, nail shapping, cuticle care + callus removal, exfoliate with softening lemon sugar scrub, hot towel. Finish with moisturizing with lotion massage and polish.': '$28',
+            
+            'Combo Deluxe': '$45',
+            '3. Classic Deluxe Plus Pedicure: Pepper mint soak in, nail shapping, cuticle care + callus removal, exfoliate with softening lemon sugar scrub + Foot Mask, hot towel. Finish with moisturizing with lotion massage and polish.': '$32',
+            
+            'Combo Deluxe Plus' : '$52',
+            '4. Deluxe OPI Pedicure: Pepper mint soak in, nail shapping, cuticle care + callus removal, exfoliate with softening OPI sugar scrub + Foot mask, hot towel. Finish with moisturizing with OPI lotion massage and polish.': '$34',
+            
+            'Combo Duluxe OPI': '$58',
+            '5. Deluxe OPI Pedicure w/Paraffin: Pepper mint soak in, nail shapping, cuticle care + callus removal, exfoliate with softening OPI sugar scrub + Foot mask + Paraffin wax, hot towel. Finish with moisturizing with OPI lotion massage and polish.': '$40',
+            
+            'Combo Deluxe OPI W/Paraffin Wax': '$65',
+            '6. Luxury OPI Pedicure: Pepper mint soak in, nail shapping, cuticle care + callus removal, exfoliate with softening OPI sugar scrub + Foot Mask + Paraffin wax, hot towel, hot stone massage to releaseiles and muscle and massage OPI lotion and polish.': '$50',
+            
+            'Combo Luxury OPI':'$75',
+            '7. Lush Nail Special Nature: Sea salt soak, mud masque, massage cream, sugar scrub, hot towel': '$40+',
+            
+            '8. VIP Luxury Nature: Sea salt soak, sugar scrub, hot towel, masque massage cream, paraffin wax, hot stone, massage.': '$50+'
+        },
+        children:{
+            '10 Year old and under':'',
+            'Kid Manicure': '$10+',
+            'Kid Pedicure (under 4 ft.)': '$18+',
+            'Kid Pedicure (over 4 ft.)':'$23+',
+            'Kid Manicure & Pedicure (under 4 ft.)': '$28+',
+            'Kid Nail Art (under 4 ft.)': '$4+',
+            'Nail Polish (under 4 ft.)':'$5+',
+            'Toe Polish (under 4 ft.)':'$5+'
+            
+        },
+        waxing:{
+            'Eyebrows':'$9',
+            'Lip':'$5',
+            'Chin':'$7+',
+            'Whole Face':'$30+',
+            'Underarms':'$20+',
+            'Half Arms':'$35+',
+            'Half Legs':'$42+',            
+        },
+        eyelash:{
+            'Individual Lash': '$25',
+            'Eyelashes Whole Strip':'$16',
+            'Eyelash Extensions':'$120+',
+            'Eyelashes Fill':'$50+',
         }
     }
     reply.view('/views/home/index', priceList)//{text:'text'})
--- views/price-list/lib.js
+++ views/price-list/lib.js
@@ -1,30 +1,35 @@
 'use strict'
 
 
+
 let crrtActiveLiElement = document.getElementById('ul-priceList').childNodes[0]
-let crrtShowIdElement = document.getElementById('acrylic')
+let crrtDisplayId = '#acrylic'
 
 $('.price-link').on("click", function(){
-    //remove active from current active li
-    crrtActiveLiElement.classList.remove('active')
     //get href query string
     const $thisHref = $(this).attr("href")
     var numberSymbolPosition = $thisHref.search('#')
     const qryString = $thisHref.substring(numberSymbolPosition+1, numberSymbolPosition.length)
+
+    /**for the navi */
+    //remove active from current active li
+    crrtActiveLiElement.classList.remove('active')
+
     //add active to new current active li
-
     var index = getChildNodeIndex('ul-priceList', $thisHref)
-
     crrtActiveLiElement = document.getElementById('ul-priceList').childNodes[index]
     console.log(crrtActiveLiElement)
     crrtActiveLiElement.classList.add('active')
+    /**end the navi */
 
-    //add hidden class to current Id s    
-    crrtShowIdElement.classList.add('hidden')
-    //remove hidden from new current ID
-    crrtShowIdElement = document.getElementById(qryString)
-    crrtShowIdElement.classList.remove('hidden')
-    
+    /** for show and hide the menu */
+    //hide the current display by id
+    document.getElementById(crrtDisplayId).className = "row hidden"
+    crrtDisplayId = $thisHref
+
+    //display the clicked id
+    document.getElementById(crrtDisplayId).className = "row"   
+    /** end show and hide the menu */ 
 })
 
 function getChildNodeIndex(parentId, href){
