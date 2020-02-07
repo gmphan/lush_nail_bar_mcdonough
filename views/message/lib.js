bb2165e2999f9d0e40e271ea2e3927ea07e0ad62
Giang Phan <giangphan@clayton.edu>
2020-02-07 16:30:41 -0500

    separate components

---
+++ views/message/lib.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+$('#email-form').submit(async function(e){
+    e.preventDefault();      
+    let data = {
+        name:$("#name").val(),
+        email:$('#email').val(),
+        phone:$('#phone').val(),
+        message:$('#message-text').val()
+    }
+    // document.getElementById('email-form').reset();
+    
+    let ajaxRes = await ajaxFunc('POST', '/mailer', data)
+    console.log(ajaxRes)
+    if(ajaxRes === 1){
+        alert('We have recieved your message. Thank you '+data.name+'!')
+        document.getElementById('email-form').reset();
+    }
+});    
+    // });
+    
+    
