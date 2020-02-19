8af0c78ff43400c41bd4e854d3c7e42d7eb75487
Giang Phan <giangphan@clayton.edu>
2020-02-19 12:00:18 -0500

    add href to redirect from gallery

--- views/gallery/lib.js
+++ views/gallery/lib.js
@@ -2,13 +2,11 @@
 //get homeUrl from the session 
 var homeUrl = sessionStorage.homeUrl
 $(".nav-link").on("click", function redirectPage(){
-    //figure out if it is still in the home page then do nothing
-    
+    //figure out if it is still in the home page then do nothing    
     console.log('home url from nav', homeUrl)
     if(window.location.href !== homeUrl){
         const hrefValue = $(this).attr("href")
-        console.log(homeUrl) 
-        window.location.href = homeUrl//+hrefValue 
+        window.location.href = homeUrl+hrefValue 
     }        
 })   
 
