4c015ff6c0208967ea908cbee35b04f099cc397f
Giang Phan <oc_buu@hotmail.com>
2020-03-06 06:04:49 -0500

    fixed typo of hrefholder to hrefHolder

--- views/gallery/lib.js
+++ views/gallery/lib.js
@@ -2,8 +2,7 @@
 //get homeUrl from the session which capture from home/lib.js
 var homeUrl = sessionStorage.homeUrl
 $(".nav-link").on("click", function redirectPage(){
-    //figure out if it is still in the home page then do nothing    
-    console.log('home url from nav', homeUrl)
+    //figure out if it is still in the home page then do nothing      
     if(window.location.href !== homeUrl){
         const hrefValue = $(this).attr("href")
         sessionStorage.setItem('hrefHolder', hrefValue)
--- views/home/lib.js
+++ views/home/lib.js
@@ -1,17 +1,28 @@
 
+/**
+ * THINGS TO REMEMBER:
+ * Since the original design was for one page website but I customized it to have
+ * extra page (gallery), more javascript was added to the page to fix the navigation function
+ * REMEMBER to put javascript on the relevant lib of each view. 
+ */
+
 $(document).ready(async function(){  
     var homeUrl = getHomeUrl()
     console.log('home URL:', homeUrl)
+
     //session will keep a value from page to page
-    sessionStorage.setItem("homeUrl", homeUrl)
+    sessionStorage.setItem("homeUrl", homeUrl) 
 
     //check to see if this page was a come back from another page beside the homepage
-    //get the rolling to work from a menu page to homepage
     if(typeof sessionStorage.hrefHolder !== 'undefined'){
+
+        //doing the special rolling one time instead of modified original code
         $('html, body').stop().animate({
             scrollTop: ($(sessionStorage.getItem('hrefHolder')).offset().top - 50)
         }, 1250, 'easeInOutExpo');
-        sessionStorage.setItem('hrefholder', "");
+
+        //need to reset the sessionStorage for hrefHolder, so it won't go back to the last hrefHolder when refresh the page
+        sessionStorage.setItem('hrefHolder', '');
     }
 });
 
