bb83573615f678d73d5f34d56ce95bc64522a689
Giang Phan <giangphan@clayton.edu>
2020-05-12 09:25:21 -0400

    init added to new ec2

--- views/gallery/lib.js
+++ views/gallery/lib.js
@@ -10,4 +10,13 @@ $(".nav-link").on("click", function redirectPage(){
     }        
 })   
 
+/**
+ * The reason to have URL capture in sessionStorage because of a few things:
+ * 1. The original home page was a free one page web from bootrap, and it use #menu in
+ * its navigation, so the page can go back and forth with the browser.
+ * 2. Gallery page is an another view and completely separate from the main page, and 
+ * to use the same navigation, I have to use the code block above to hit the server again
+ * to go back to the homepage 
+ */
+
 
