838bbd8d171e1fcff4a63127ae507f329c016fb9
Giang Phan <giangphan@clayton.edu>
2020-08-18 14:02:00 -0400

    fixed spelling for the form

--- public/assets/covid19consent.js
+++ public/assets/covid19consent.js
@@ -46,10 +46,10 @@ async function createHtml(newInputObj){
             <p>
                 I,
                 ${inputObj.input1}
-                , knowingly and willingly consent to have nail treatment during the COVID-19 pandemic and will not hold anyone of Lush Nail Bar Mc Donough liable for the possibility of contrating COVID-19.<br><br>
-                I understand the COVID-19 virus has a long incubation period during which carriers of the visu may not show synmtoms and still be hightly contagious. It is impossible to determine who has it and who does not given the current limits in virus testing.
+                , knowingly and willingly consent to have nail treatment during the COVID-19 pandemic and will not hold anyone of Lush Nail Bar McDonough liable for the possibility of contracting COVID-19.<br><br>
+                I understand the COVID-19 virus has a long incubation period during which carriers of the visus may not show synmtoms and still be hightly contagious. It is impossible to determine who has it and who does not given the current limits in virus testing.
                 ${inputObj.input2} (Initial)<br><br>
-                I understand that due to the frequency of visits of other clients, the characteristics of the virus, and the characteristics of the nail treatments, that I have a elevated rish of contracting the virus simply by being in a salon. 
+                I understand that due to the frequency of visits of other clients, the characteristics of the virus, and the characteristics of the nail treatments, that I have a elevated risk of contracting the virus simply by being in the salon. 
                 ${inputObj.input3} (Initial)<br><br>
                 I confirm that I am not presenting any of the following symptoms of COVID-19 listed below: 
                 ${inputObj.input4} (Initial)<br>
@@ -65,7 +65,7 @@ async function createHtml(newInputObj){
                 <li>Sore Throat</li>			
                 </ul>
                 <br>
-                To prevent the spread of contangious of the viruses and to help protect each other, I understand that I will have to follow the salon's strict guidelines.
+                To prevent the spread of contangious of the virus and to help protect each other, I understand that I will have to follow the salon's strict guidelines.
                 ${inputObj.input6}
                 (Initial)<br><br>
                 I understand that air travel significantly increases my risk of contracting and transmitting the COVID-19 virus, and I understand that the CDC, OSHA and Georgia Board of Cosmetology and Barbers recommend social distancing of at least six feet.
